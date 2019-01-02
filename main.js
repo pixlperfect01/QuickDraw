85dd49001aebc82541c015cdbc62e336a17f7f2b
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-02 16:08:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
+var c,ctx,data;
+var mouseData={
+  down:true,
+};
+function setup(){
+  c=document.getElementById("canvas");
+  ctx=c.getContext("2d");
+  data=[];
+  loop();
+  c.addEventListener("mousemove",function(event){
+    test(event);
+  });
+  c.addEventListener("mousedown",function(){
+    data.push(new stroke());
+    mouseData.down=true;
+  });
+  c.addEventListener("mouseup",function(){
+    mouseData.down=false;
+  });
+}
 
+
+
+
+function test(m){
+  if(mouseData.down){
+    ctx.fillRect(m.offsetX, m.offsetY, 1, 1);
+    data[data.length-1].x.push(m.offsetX);
+    data[data.length-1].y.push(m.offsetY);
+}
+
+
+function stroke(){
+  var time=new Date();
+  this.t="Date = "+time.getFullYear+", "+time.getMonth()+", "+time.getDay()+"   Time = "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()+":"+time.getMilliseconds();
+  this.x=[];
+  this.y=[];
+}
