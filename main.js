b9071c13880dbefe54054f1ad6959fe761a0761a
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-02 18:02:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var c,ctx,data;
 var mouseData={
   down:false,
 };
+var settings={
+  backgroundColor:"#FFFFFF",
+  strokeColor:"#000000",
+  strokeSize:7,
+  currentMode:"draw"
+};
+setTimeout(update, 100);
 function setup(){
   c=document.getElementById("canvas");
   c.width=window.innerWidth-18;
@@ -25,23 +32,31 @@ function setup(){
   });
 }
 
-
-
-
 function test(m){
   if(mouseData.down){
     ctx.beginPath();
-    ctx.arc(m.offsetX, m.offsetY, 7, 0, Math.PI*2);
+    ctx.fillStyle=settings.strokeSize
+    ctx.arc(m.offsetX, m.offsetY, settings.strokeSize, 0, Math.PI*2);
     ctx.fill();
     data[data.length-1].xs.push(m.offsetX);
     data[data.length-1].ys.push(m.offsetY);
+    data[data.length-1].ty.push(settings.strokeColor);
+    data[data.length-1].ty.push(settings.currentMode);
   }
 }
-
 
 function stroke(){
   var time=new Date();
   this.timeStamp="Date = "+(time.getMonth()+1)+", "+time.getDate()+", "+time.getFullYear()+"   Time = "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()+":"+time.getMilliseconds();
   this.xs=[];
   this.ys=[];
+  this.co=[];
+  this.ty=[];
+}
+
+function update(){
+  settings.backgroundColor=document.getElementById("backColor").value;
+  settings.strokeColor=document.getElementById("strokeColor").value;
+  settings.strokeSize=document.getElementById("strokeSize").value;
+  settings.currentMode=document.getElementById("mode").value;
 }
